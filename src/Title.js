fdbe13306262c3a747f68ca2685e4c96c63a73b6
oditha14@gmail.com <62847596+odis43@users.noreply.github.com>
2022-04-16 13:14:26 -0400

    basic title component

---
+++ src/Title.js
@@ -0,0 +1,12 @@
+import { getByTitle } from '@testing-library/react'
+import React from 'react'
+
+function Title() {
+    return (
+        <div className="Title">
+            <h1>This is our project</h1>
+        </div>
+    )
+}
+
+export default Title 
