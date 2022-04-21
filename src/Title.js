04f498113bf180a5ba39bd94beabff5327b703dc
Hannah-C <choihannah2003@gmail.com>
2022-04-21 14:04:01 -0400

    nruh

--- src/Title.js
+++ src/Title.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Title() {
     return (
         <div className="Title">
-            <h1>Hannah Oditha</h1>
+            <h1>Hannah Choi</h1>
         </div>
     )
 }
