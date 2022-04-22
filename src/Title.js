b018e0665dc8f822587cbf867f4c478b1eda5822
oditha14@gmail.com <62847596+odis43@users.noreply.github.com>
2022-04-22 02:38:44 -0400

    new hover mechanic

--- src/Title.js
+++ src/Title.js
@@ -1,13 +1,43 @@
-import logo from './dalogo';
 import React from 'react'
 import './Title.css';
 
-function Title() {
+class Title extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+       imgSrc: require('./dalogo')
+    };
+    this.handleMouseOver = this.handleMouseOver.bind(this);
+    this.handleMouseOut = this.handleMouseOut.bind(this);
+  }
+
+  handleMouseOver() {
+    this.setState({
+      imgSrc: require('./difflogo')
+    });
+  }
+
+  handleMouseOut() {
+    this.setState({
+      imgSrc: require('./dalogo')
+    });
+  }
+
+  render() {
     return (
-        <div className='logo'>
-             <img src= {logo} width={650} height={500} alt='logo' />
-        </div>
+      <div className='Logo'>
+        <img onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut} src={this.state.imgSrc} width = {1250} heigth = {1000}/>
+      </div>
     );
+  }
+
 }
 
-export default Title 
+Title.propTypes = {
+}
+
+Title.defaultProps = {
+}
+
+export default Title;
