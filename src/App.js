5d830e054515b7e97d1ad31f422865d13c2bc79f
Hannah-C <choihannah2003@gmail.com>
2022-04-22 11:15:10 -0400

    change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
+//import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import bgimg from './image/drawdleimg.png'
+
 import Title from './Title';
 import './Title.css';
-function App() {
+class App extends Component {
+  render() {
+    const myStyle={
+      backgroundImage: "url('http://localhost:3000/paper.png')",
+      height: '100h',
+      marginTop: '-70px',
+      fontSize: '50px',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+    };
   return (
-    <div className="App">
-      <Title/>
+    <div style={myStyle}>
+      <div className="App">
+        <Title/>
+      </div>
     </div>
   );
+}
 }
 
 export default App;
