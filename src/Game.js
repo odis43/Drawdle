3a8b87a698aca73c4beebb750f9c60bdfa9af887
oditha14@gmail.com <62847596+odis43@users.noreply.github.com>
2022-04-23 15:27:44 -0400

    progress

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,14 @@
 import React, {Component} from "react";
 import './Game.css';
+import Box from '@mui/material/Box';
+import { ReactSketchCanvas } from 'react-sketch-canvas';
+
+  const styles = {
+    
+  };
 
     class Game extends Component {
+      
         render() {
           const myStyle={
             backgroundImage: "url('http://localhost:3000/paper.png')",
@@ -10,13 +17,21 @@ import './Game.css';
             backgroundSize: 'cover',
             backgroundRepeat: 'no-repeat'
           };
-          
-        return (
-          <div style={myStyle}>
-            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}} className = 'Gamebox'>
-            
-            </div>
 
+        return (
+        
+          <div style={myStyle}>
+            <div className = 'Gamebox' style={{}}>
+            <ReactSketchCanvas 
+            width="100"
+            height="100"
+            strokeWidth={5}
+            background  
+            strokeColor="black"
+            canvasColor =  "transparent"
+            style = {myStyle}
+            />
+            </div>
           </div>
         );
       }
