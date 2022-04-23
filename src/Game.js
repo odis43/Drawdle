0e0d151d003b68b8dc1b3038cd1cecaf85ffe6cc
oditha14@gmail.com <62847596+odis43@users.noreply.github.com>
2022-04-23 11:47:03 -0400

    commit box

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,6 @@
 import React, {Component} from "react";
+import './Game.css';
+import Box from '@mui/material/Box';
 
     class Game extends Component {
         render() {
@@ -9,10 +11,35 @@ import React, {Component} from "react";
             backgroundSize: 'cover',
             backgroundRepeat: 'no-repeat'
           };
+          
+          const startDrawing = () => {
+
+          }
+
+          const finishDrawing = () => {
+
+          }
+
+          const draw = () => {
+
+          }
         return (
           <div style={myStyle}>
-            <div className="Game">
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}} className = 'Gamebox'>
+            <Box
+              sx={{
+              width: 500,
+              height: 500,
+              backgroundColor: "white",
+              opacity: [1, 1, 1]
+            }}  
+            />
+             <canvas
+                onMouseDown = {startDrawing}
+                onMouseUp = {finishDrawing}
+              />
             </div>
+
           </div>
         );
       }
