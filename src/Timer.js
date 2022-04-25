217a746a23d97fc27b8141fb6bba83947ccf41a9
oditha14@gmail.com <62847596+odis43@users.noreply.github.com>
2022-04-24 20:45:26 -0400

    sddfdsf

---
+++ src/Timer.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import './Timer.css';
+import { Redirect } from 'react-router-dom';
+
+export default class Timer extends Component {
+    
+    constructor (props) {
+        super(props);
+        this.state = {
+            seconds: 3,
+            off: false
+        };
+    }
+
+    componentDidMount() {
+        this.myInterval = setInterval(() => {           
+                if(this.state.seconds !== 0) {
+                    this.setState(({seconds}) => ({
+                        seconds: seconds - 1
+                    }))
+                }
+            
+        }, 1000)
+    }
+
+    componentDidUpdate(){
+        if(this.state.seconds === 0){
+           window.location.href = "Share"
+        }
+    }
+    
+
+    render() {
+        return (
+            <span>
+               {this.state.seconds}
+            </span>
+        );
+    }
+}
