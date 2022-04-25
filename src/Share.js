1e27db6cd694c3e916c5a719cef5801e69353e29
oditha14@gmail.com <62847596+odis43@users.noreply.github.com>
2022-04-24 20:44:27 -0400

    bsodfbsdf

---
+++ src/Share.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Share extends React.Component {
+
+    render() {
+        return (
+            <h1>Hello there my name is bob</h1>
+        );
+    }
+
+
+}
+
+export default Share
