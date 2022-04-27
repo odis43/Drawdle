3899ac88100880254d4f096e7916b4069bcb784d
oditha14@gmail.com <62847596+odis43@users.noreply.github.com>
2022-04-26 20:53:00 -0400

    playhover

--- src/playHoverProvider.js
+++ src/playHoverProvider.js
@@ -1,14 +1,16 @@
-export const MContext = React.createContext();
+import React, { Component } from 'react';
+import PlayButton from './PlayButton';
 
-  class playHoverProvider extends Component {
-    state = {message: './difflogo'}
-     render() {
-        return (
-            <MContext.Provider value={
-            {   state: this.state,
-                setMessage: (value) => this.setState({
-                            message: value })}}>
-            {this.props.children}   //this indicates that all the child tags with MyProvider as Parent can access the global store.
-            </MContext.Provider>)
-    }
+class playHoverProvider extends Component {
+
+  Parentfunction () {
+    this.PlayButton.beinghovered();
+  }
+
+  render() {
+    return <PlayButton ref={component => this.PlayButton = component} />
+  }
 }
+
+export default playHoverProvider
+
