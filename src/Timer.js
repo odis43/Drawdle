d416837f454df894e481d85b9e44b7a4e2e7e57d
oditha14@gmail.com <62847596+odis43@users.noreply.github.com>
2022-04-26 20:53:17 -0400

    timerjs

--- src/Timer.js
+++ src/Timer.js
@@ -7,7 +7,7 @@ export default class Timer extends Component {
     constructor (props) {
         super(props);
         this.state = {
-            seconds: 3,
+            seconds: 180,
             off: false
         };
     }
