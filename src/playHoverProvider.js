e3f40eddbfd1c87f7ca31913e7f6ff7715fa4b61
Hannah-C <choihannah2003@gmail.com>
2022-04-28 12:23:34 -0400

    I meant this one to delete

---
+++ src/playHoverProvider.js
@@ -0,0 +1,16 @@
+//i dont think we need this file
+
+export const MContext = React.createContext();
+
+  class playHoverProvider extends Component {
+    state = {message: './difflogo'}
+     render() {
+        return (
+            <MContext.Provider value={
+            {   state: this.state,
+                setMessage: (value) => this.setState({
+                            message: value })}}>
+            {this.props.children}   //this indicates that all the child tags with MyProvider as Parent can access the global store.
+            </MContext.Provider>)
+    }
+}
